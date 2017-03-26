6a53475620bcd5497662bbb714e814fdb0623c5c
i4b1n1 <zdarzeniowka@gmail.com>
2017-03-26 12:56:31 +0200

    adc

---
+++ src/main/java/com/example/PierwszyApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PierwszyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PierwszyApplication.class, args);
+	}
+}
